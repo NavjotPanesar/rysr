ec1dd3f4f82f335a6f4c1a1ac07ac401b0e7f7e4
Aaron Lobo <lobo.aaron1@gmail.com>
2015-09-19 18:19:18 -0400

    Animate logo screen

--- android/app/src/main/java/co/rysr/rysr/intro/LogoFragment.java
+++ android/app/src/main/java/co/rysr/rysr/intro/LogoFragment.java
@@ -5,12 +5,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import co.rysr.rysr.R;
 
 
 public class LogoFragment extends Fragment {
-     /**
+    @Bind(R.id.imageViewIcon) ImageView imageViewIcon;
+    @Bind(R.id.imageViewLogoTypography) ImageView imageViewLogoTypography;
+
+    /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      * @return A new instance of fragment LogoFragment.
@@ -32,6 +40,17 @@ public class LogoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_logo, container, false);
+        View view = inflater.inflate(R.layout.fragment_logo, container, false);
+        ButterKnife.bind(this, view);
+
+        // Setup animations
+        Animation slideUp = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_up);
+        Animation slideDown = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_down);
+        slideDown.setStartOffset(300);
+
+        imageViewIcon.startAnimation(slideUp);
+        imageViewLogoTypography.startAnimation(slideDown);
+
+        return view;
     }
 }
