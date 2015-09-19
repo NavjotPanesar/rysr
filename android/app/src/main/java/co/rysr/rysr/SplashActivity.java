c9225e3a2124bfbba1d9c8b74bf29189f929af28
Aaron Lobo <lobo.aaron1@gmail.com>
2015-09-19 16:54:22 -0400

    Add AppIntro and LogoFragment

--- android/app/src/main/java/co/rysr/rysr/SplashActivity.java
+++ android/app/src/main/java/co/rysr/rysr/SplashActivity.java
@@ -1,43 +1,54 @@
 package co.rysr.rysr;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
 import co.rysr.rysr.Activity.BaseActionBarActivity;
+import co.rysr.rysr.intro.LogoFragment;
 
-public class SplashActivity extends BaseActionBarActivity {
+public class SplashActivity extends AppIntro {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void init(Bundle savedInstanceState) {
+
+        // Add your slide's fragments here
+        // AppIntro will automatically generate the dots indicator and buttons.
+        addSlide(LogoFragment.newInstance());
+//        addSlide(second_fragment);
+//        addSlide(third_fragment);
+//        addSlide(fourth_fragment);
+
+        // Instead of fragments, you can also use our default slide
+        // Just set a title, description, background and image. AppIntro will do the rest
+        addSlide(AppIntroFragment.newInstance("test", "description", R.drawable.ic_arrow_forward_white_24px, Color.RED));
+
+        // Override bar/separator color
+        setBarColor(getResources().getColor(R.color.primary_dark));
+        setSeparatorColor(Color.parseColor("#2196F3"));
+
+        // Hide Skip/Done button
+        showSkipButton(false);
+        showDoneButton(false);
+
+        // Turn vibration on and set intensity
+        // NOTE: you will probably need to ask VIBRATE permesssion in Manifest
+        setVibrate(true);
+        setVibrateIntensity(30);
     }
 
     @Override
-    protected int getLayoutId() {
-        return R.layout.activity_splash;
+    public void onSkipPressed() {
+        // Do something when users tap on Skip button.
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onDonePressed() {
+        // Do something when users tap on Done button.
     }
 }
---
+++ android/app/src/main/java/co/rysr/rysr/intro/LogoFragment.java
@@ -0,0 +1,37 @@
+package co.rysr.rysr.intro;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.rysr.rysr.R;
+
+
+public class LogoFragment extends Fragment {
+     /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     * @return A new instance of fragment LogoFragment.
+     */
+    public static LogoFragment newInstance() {
+        return new LogoFragment();
+    }
+
+    public LogoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_logo, container, false);
+    }
+}
