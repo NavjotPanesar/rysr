6b07834c14a28a10a7f7f5df714f096e06584d54
Navjot <NavjotSPanesar@gmail.com>
2015-09-20 03:53:55 -0400

    Fixed my graph 8^)

--- android/app/src/main/java/co/rysr/rysr/Activity/MainActivity.java
+++ android/app/src/main/java/co/rysr/rysr/Activity/MainActivity.java
@@ -1,5 +1,6 @@
 package co.rysr.rysr.Activity;
 
+import android.app.FragmentTransaction;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothAdapter.LeScanCallback;
 import android.bluetooth.BluetoothDevice;
@@ -9,6 +10,7 @@ import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Menu;
@@ -58,6 +60,12 @@ public class MainActivity extends BaseActionBarActivity{
         });
 
        ArduinoConnection.onCreate();
+
+        if (savedInstanceState == null) {
+            Fragment fragment = new MainFragment().newInstance();
+            android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.container, fragment).commit();
+        }
     }
 
     // OnResume, called right before UI is displayed.  Start the BTLE connection.
--- android/app/src/main/java/co/rysr/rysr/Fragment/MainFragment.java
+++ android/app/src/main/java/co/rysr/rysr/Fragment/MainFragment.java
@@ -24,12 +24,12 @@ import co.rysr.rysr.Utils.ArduinoConnection;
 /**
  * Created by Navjot on 9/19/2015.
  */
-public class MainFragment extends Fragment{
+public class MainFragment extends android.support.v4.app.Fragment {
 
     @Bind(R.id.chart)
     LineChart chart;
 
-   // ArduinoConnection ac;
+    // ArduinoConnection ac;
 
     int counter = 5;
 
@@ -38,17 +38,12 @@ public class MainFragment extends Fragment{
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-      /*  if(ac == null) {
-            ac = new ArduinoConnection(getActivity(), bluetoothRecievedListener);
-        }
-        ac.onCreate();*/
-
         ButterKnife.bind(this, rootView);
 
         final ArrayList<Entry> yVals = new ArrayList<>();
         final LineDataSet data = new LineDataSet(yVals, "X-axis");
         final ArrayList<String> xVals = new ArrayList<String>();
-        for(int i = 1; i <= 100; i++){
+        for (int i = 1; i <= 100; i++) {
             xVals.add(String.valueOf(i));
         }
         final LineData finalData = new LineData(xVals, data);
@@ -62,13 +57,17 @@ public class MainFragment extends Fragment{
         return rootView;
     }
 
+    public android.support.v4.app.Fragment newInstance(){
+        return new MainFragment();
+    }
+
     private void feedMultiple() {
 
         new Thread(new Runnable() {
 
             @Override
             public void run() {
-                for(int i = 0; i < 50000; i++) {
+                for (int i = 0; i < 50000; i++) {
 
                     getActivity().runOnUiThread(new Runnable() {
 
@@ -131,17 +130,10 @@ public class MainFragment extends Fragment{
     @Override
     public void onResume() {
         super.onResume();
-        /*if(ac == null){
-            ac = new ArduinoConnection(getActivity(), null);
-            ac.onCreate();
-        }
-
-        ac.onResume();*/
     }
 
     @Override
     public void onStop() {
         super.onStop();
-   //     ac.onResume();
     }
 }
